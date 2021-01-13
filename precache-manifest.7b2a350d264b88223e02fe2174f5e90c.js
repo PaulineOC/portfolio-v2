51d70feac2a7c72a6dac91c992f234687d2b7211
PaulineOC <poc215@nyu.edu>
2021-01-13 15:00:25 -0500

    Updates

---
+++ precache-manifest.7b2a350d264b88223e02fe2174f5e90c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b5a19f2652bbdcdb1d3dc6594219cff",
+    "url": "/portfolio-v2/index.html"
+  },
+  {
+    "revision": "14a607f1580e51bebf92",
+    "url": "/portfolio-v2/static/css/main.3760a3ba.chunk.css"
+  },
+  {
+    "revision": "db96998a9a40ed8b2498",
+    "url": "/portfolio-v2/static/js/2.0c4ba4c8.chunk.js"
+  },
+  {
+    "revision": "3ae8240266c7f67b942dab69ea230db8",
+    "url": "/portfolio-v2/static/js/2.0c4ba4c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a607f1580e51bebf92",
+    "url": "/portfolio-v2/static/js/main.392dc7e8.chunk.js"
+  },
+  {
+    "revision": "58daaf2044f28a9eccfa",
+    "url": "/portfolio-v2/static/js/runtime-main.a8d6ddc0.js"
+  },
+  {
+    "revision": "52c37a83ad720c9503665d2db31a05ef",
+    "url": "/portfolio-v2/static/media/Title.52c37a83.png"
+  }
+]);
